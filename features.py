bc6664304f7520337c41d41c3341f2e006379899
djokjulapfe <djordjemarjanovic998@gmail.com>
2020-01-10 08:52:43 +0100

    Added Stacked and Multiply features

--- features.py
+++ features.py
@@ -9,15 +9,14 @@ class Feature:
 
 class RectSelect(Feature):
 
-    def __init__(self, corner: tuple, shape: tuple = (1, 1)):
-        self.corner = corner
-        self.shape = shape
+    def __init__(self, up, down, left, right):
+        self.up = up
+        self.down = down
+        self.left = left
+        self.right = right
 
     def get_feature(self, x: np.ndarray):
-        return x[
-               self.corner[0]: self.corner[0] + self.shape[0],
-               self.corner[1]: self.corner[1] + self.shape[1],
-               ]
+        return x[self.up: self.down, self.left: self.right]
 
 
 class Sum(Feature):
@@ -26,11 +25,30 @@ class Sum(Feature):
         return np.sum(x)
 
 
+class Multiply(Feature):
+
+    def __init__(self, factor: float):
+        self.factor = factor
+
+    def get_feature(self, x: np.ndarray):
+        return x * self.factor
+
+
 class LinearFilter(Feature):
 
     def __init__(self, kernel: np.ndarray):
-        super().__init__()
         self.kernel = kernel
 
     def get_feature(self, x: np.ndarray):
         return cv2.filter2D(x, -1, self.kernel)
+
+
+class Stack(Feature):
+    def __init__(self, sub_extractors: list):
+        self.sub_extractors = sub_extractors
+
+    def get_feature(self, x: np.ndarray):
+        ret = x
+        for extractor in self.sub_extractors:
+            ret = extractor.get_feature(ret)
+        return ret
